6bacab4a49a0fe0bd39d197e922eaa79ac38ba01
khaledarafa <arafaarranger@gmail.com>
2025-10-01 19:26:53 +0300

    first commit

---
+++ public/js/register-sw.js
@@ -0,0 +1,11 @@
+// public/js/register-sw.js
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', async () => {
+      try {
+        const reg = await navigator.serviceWorker.register('/sw.js');
+        console.log('SW registered:', reg);
+      } catch (err) {
+        console.error('SW register failed', err);
+      }
+    });
+  }
