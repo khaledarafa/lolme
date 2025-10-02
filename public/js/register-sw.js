7b012098016d00868b9a1971ebb2d29d9b8a59f1
khaledarafa <arafaarranger@gmail.com>
2025-10-02 16:59:44 +0300

    manifest

--- public/js/register-sw.js
+++ public/js/register-sw.js
@@ -2,17 +2,30 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", async () => {
     try {
-      const reg = await navigator.serviceWorker.register("/sw.js");
-      console.log("SW registered:", reg);
+      if (window.location.hostname !== "localhost") {
+        const reg = await navigator.serviceWorker.register("/sw.js");
+        console.log("SW registered:", reg);
+      }
     } catch (err) {
       console.error("SW register failed", err);
     }
   });
 }
 
-if ("serviceWorker" in navigator) {
-  // نعلق التسجيل أثناء التطوير
-  if (window.location.hostname !== "localhost") {
-    navigator.serviceWorker.register("/sw.js");
-  }
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", async () => {
+//     try {
+//       const reg = await navigator.serviceWorker.register("/sw.js");
+//       console.log("SW registered:", reg);
+//     } catch (err) {
+//       console.error("SW register failed", err);
+//     }
+//   });
+// }
+
+// if ("serviceWorker" in navigator) {
+//   // نعلق التسجيل أثناء التطوير
+//   if (window.location.hostname !== "localhost") {
+//     navigator.serviceWorker.register("/sw.js");
+//   }
+// }
