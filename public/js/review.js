96da81661be7e259fe7b78bf85bca5cc7d40a20f
Khaled Arafa <arafaarranger@gmail.com>
2026-04-12 21:58:20 +0200

    logo

--- public/js/review.js
+++ public/js/review.js
@@ -3,14 +3,14 @@ import { db } from "/js/firebase.js";
 import { collection, query, where, getDocs, updateDoc, doc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// const password = "lolo"; // غيرها براحتك
+const password = "lolo"; // غيرها براحتك
 
-// const userPass = prompt("ادخل كلمة مرور الأدمن:");
+const userPass = prompt("ادخل كلمة مرور الأدمن:");
 
-// if (userPass !== password) {
-//   document.body.innerHTML = "<h1>🚫 مش مسموح يا نجم</h1>";
-//   throw new Error("Unauthorized");
-// }
+if (userPass !== password) {
+  document.body.innerHTML = "<h1>🚫 مش مسموح يا نجم</h1>";
+  throw new Error("Unauthorized");
+}
 const box = document.getElementById("questions-box");
 
 async function loadQuestions() {
@@ -54,7 +54,7 @@ div.innerHTML = `
   <h3>${data.text}</h3>
 
   <p>🧠 النوع: ${data.type === "choice" ? "اختيارات" : "كتابة"}</p>
-
+  
   <p>📂 ${data.category}</p>
 
   ${optionsHTML}
